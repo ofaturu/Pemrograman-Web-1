9ad68d366efc260a785a3157282db46fec1a4023
Muchamad Fatchur Rachman <fatchurrachman001@gmail.com>
2026-04-23 13:23:33 +0700

    belajar PHP dan update tugas 1

---
+++ belajarPHP/biodata.php
@@ -0,0 +1,10 @@
+<?php
+    $nim = '20220001';
+    $nama = 'Joni';
+    $alamat = 'Jl. Slamet riyadi No.26';
+    $nilai = 90;
+    echo "NIM : ", $nim, "<br>";
+    echo "Nama : ", $nama, "<br>";
+    echo "Alamat : ", $alamat, "<br>";
+    echo "Nilai : $nilai";
+?>
